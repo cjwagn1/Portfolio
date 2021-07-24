9979687457915fd5724c2d71a42685f9cad9a83a
Carter Wagner <cjwagn1@gmail.com>
2021-07-23 23:37:01 -0500

    Fixed client/server side temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Route, Switch } from "react-router-dom";
 import styled, { AnyStyledComponent } from "styled-components";
 import Home from "./components/sections/Home";
@@ -9,49 +9,49 @@ import ACM from "./components/sections/ACM";
 import Pick from "./components/sections/Pick";
 import CRM from "./components/sections/CRM";
 const Group: AnyStyledComponent = styled.div`
-	display: flex;
-	flex-direction: column;
-	font-family: "Poppins", sans-serif;
-	width: 100%;
+  display: flex;
+  flex-direction: column;
+  font-family: "Poppins", sans-serif;
+  width: 100%;
 `;
 const Constraint: AnyStyledComponent = styled.div`
-	max-width: 60em;
-	margin-left: auto;
-	margin-right: auto;
+  max-width: 60em;
+  margin-left: auto;
+  margin-right: auto;
 `;
 const Footer: AnyStyledComponent = styled.div`
-	font-size: 20px;
-	font-weight: bold;
-	padding-top: 10px;
-	padding-bottom: 10px;
-	justify-content: center;
-	display: flex;
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  justify-content: center;
+  display: flex;
 `;
 
 const NotFound: React.FC<{}> = (): JSX.Element => {
-	return <h1>You are lost!</h1>;
+  return <h1>You are lost!</h1>;
 };
 const MainContent: React.FC<{}> = (): JSX.Element => {
-	return (
-		<Switch>
-			<Route exact={true} path="/" component={Home} />
-			<Route path="/brokenlamp" component={Brokenlamp} />
-			<Route path="/acm" component={ACM} />
-			<Route path="/pickhacks" component={Pick} />
-			<Route path="/chihuahua" component={CRM} />
-			<Route component={NotFound} />
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/brokenlamp" component={Brokenlamp} />
+      <Route path="/acm" component={ACM} />
+      <Route path="/pickhacks" component={Pick} />
+      <Route path="/chihuahua" component={CRM} />
+      <Route component={NotFound} />
+    </Switch>
+  );
 };
 export default () => {
-	return (
-		<BrowserRouter>
-			<Group>
-				<Constraint>
-					<MainContent />
-					<Footer>©2021 Carter Wagner</Footer>
-				</Constraint>
-			</Group>
-		</BrowserRouter>
-	);
+  return (
+    <HashRouter>
+      <Group>
+        <Constraint>
+          <MainContent />
+          <Footer>©2021 Carter Wagner</Footer>
+        </Constraint>
+      </Group>
+    </HashRouter>
+  );
 };
